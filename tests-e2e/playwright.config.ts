6fea4aeaadfa12167ac94a2f34fdf4d27ac3f5a1
Rurutia1027 <rurutia1027@gmail.com>
2026-09-08 19:37:41 +0100

    update end to end test cases

--- tests-e2e/playwright.config.ts
+++ tests-e2e/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: ".",
-  testMatch: "integration.spec.ts",
+  testMatch: /.*\.spec\.ts/,
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || "http://localhost:3000",
     trace: "on-first-retry",
